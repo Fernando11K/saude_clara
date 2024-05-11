40da1118fa2dfd12ec5bde6fb8222936d386e35c
Fernando11K <fernandoferreira10@hotmail.com.br>
2024-05-11 15:15:28 -0300

    chore: configura firebase no projeto e demais configuracoes para hospedagem

---
+++ src/boot/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCzxafAyO_pof4wn-NtfjAZGkZyPLGRB08",
+    authDomain: "saude-clara.firebaseapp.com",
+    databaseURL: "https://saude-clara-default-rtdb.firebaseio.com",
+    projectId: "saude-clara",
+    storageBucket: "saude-clara.appspot.com",
+    messagingSenderId: "713591281835",
+    appId: "1:713591281835:web:dadd3a845f7657c003bfee",
+    measurementId: "G-WEQ3MKKJM5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
