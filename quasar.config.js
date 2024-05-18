357122a263a2bf591c96ee97086899c5400f9ebd
Fernando11K <fernandoferreira10@hotmail.com.br>
2024-05-18 16:22:06 -0300

    feat: desabilita vite durante o build

--- quasar.config.js
+++ quasar.config.js
@@ -70,16 +70,16 @@ module.exports = configure(function (/* ctx */) {
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
 
-      vitePlugins: [
-        ['vite-plugin-checker', {
-          vueTsc: {
-            tsconfigPath: 'tsconfig.vue-tsc.json'
-          },
-          eslint: {
-            lintCommand: 'eslint "./**/*.{js,ts,mjs,cjs,vue}"'
-          }
-        }, { server: false }]
-      ]
+      // vitePlugins: [ desativei para teste
+      //   ['vite-plugin-checker', {
+      //     vueTsc: {
+      //       tsconfigPath: 'tsconfig.vue-tsc.json'
+      //     },
+      //     eslint: {
+      //       lintCommand: 'eslint "./**/*.{js,ts,mjs,cjs,vue}"'
+      //     }
+      //   }, { server: false }]
+      // ]
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
